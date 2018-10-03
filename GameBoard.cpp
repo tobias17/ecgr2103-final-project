e865915bb97d84df525cad4f80733fd7dc041c23
Tobias Fischer <tobiasfischer17@gmail.com>
2018-10-03 14:28:23 -0400

    Game Board Finishes

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -6,25 +6,84 @@
 
 #include "GameBoard.h"
 
-using namespace std;
-
 GameBoard::GameBoard(int iSize) {
     // initialize the arrays
     miSize = iSize;
-    mbaShips[iSize][iSize];
-    mbaAttacked[iSize][iSize];
-    for (int x = 0; x < iSize; x++) {
-        for (int y = 0; y < iSize; y++) {
+    for (int x = 0; x < MAX_BOARD_SIZE; x++) {
+        for (int y = 0; y < MAX_BOARD_SIZE; y++) {
             mbaShips[x][y] = false;
             mbaAttacked[x][y] = false;
+        }
+    }
+    
+    // loop through all ships
+    for (int ship = 0; ship < SHIP_AMNT; ship++) {
+        // grab how long the ship we are adding is
+        int iShipSize = ShipSizes[ship];
+        
+        // generate a vector to hold all the possible ship placements
+        vector<Point> ovPoss;
+        
+        // add all possible horizontal ship placements
+        for (int dx = 0; dx < iSize - iShipSize; dx++) {
+            for (int dy = 0; dy < iSize; dy++) {
+                bool worked = true;
+                for (int i = 0; i < iShipSize; i++) {
+                    if (mbaShips[dx + i][dy]) {
+                        worked = false;
+                        break;
+                    }
+                }
+                if (worked) {
+                    ovPoss.push_back(Point(dx, dy, Point::right));
+                }
+            }
+        }
+        
+        // add all possible vertical ship placements
+        for (int dx = 0; dx < iSize; dx++) {
+            for (int dy = 0; dy < iSize - iShipSize; dy++) {
+                bool worked = true;
+                for (int i = 0; i < iShipSize; i++) {
+                    if (mbaShips[dx][dy + i]) {
+                        worked = false;
+                        break;
+                    }
+                }
+                if (worked) {
+                    ovPoss.push_back(Point(dx, dy, Point::down));
+                }
+            }
+        }
+        
+        // add the ship from a random placement
+        srand(time(0));
+        int iIndex = rand() % ovPoss.size();
+        Point oShipPoint = ovPoss.at(iIndex);
+        for (int i = 0; i < iShipSize; i++) {
+            mbaShips[oShipPoint.x][oShipPoint.y] = true;
+            if (oShipPoint.dir == Point::right) {
+                oShipPoint.x++;
+            } else {
+                oShipPoint.y++;
+            }
         }
     }
 }
 
 bool GameBoard::getHitAndMark(int iX, int iY) {
-    
+    mbaAttacked[iX][iY] = true;
+    return mbaShips[iX][iY];
 }
 
 string GameBoard::getBoardToString() {
-    
+    stringstream sBoard;
+    for (int x = 0; x < miSize; x++) {
+        for (int y = 0; y < miSize; y++) {
+            sBoard << ((mbaAttacked[x][y]) ? ((mbaShips[x][y]) ? SHIPS_CHAR : EMPTY_CHAR) : UNKNOWN_CHAR);
+            sBoard << " ";
+        }
+        sBoard << "\n";
+    }
+    return sBoard.str();
 }
--- GameBoard.h
+++ GameBoard.h
@@ -14,18 +14,41 @@
 #ifndef GAMEBOARD_H
 #define GAMEBOARD_H
 
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <random>
+
+#define SHIPS_CHAR      "X"
+#define EMPTY_CHAR      "O"
+#define UNKNOWN_CHAR    "*"
+#define MAX_BOARD_SIZE  7
+#define SHIP_AMNT       3
+
 using namespace std;
+
+const int ShipSizes[SHIP_AMNT] = { 4, 3, 2 };
+
+class Point {
+public:
+    Point(int iX, int iY, int iDir) { x = iX; y = iY; dir = iDir; };
+    int x;
+    int y;
+    int dir;
+    enum Directions { right, down };
+};
 
 class GameBoard {
 public:
-    GameBoard(int size);
-    bool getHitAndMark(int x, int y);
+    GameBoard(int iSize);
+    bool getHitAndMark(int iX, int iY);
     string getBoardToString();
-    enum Difficulty { easy = 4, medium, hard };
+    enum Difficulty { easy = 5, medium, hard };
 private:
     int miSize;
-    bool mbaShips[][];
-    bool mbaAttacked[][];
+    bool mbaShips[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
+    bool mbaAttacked[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
 };
 
 #endif /* GAMEBOARD_H */
